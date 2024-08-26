e2f88df070b58510f66907c07bf2f75579ae93b2
Rayy-pixel <rayyanibnuhazzami@gmail.com>
2024-08-26 10:06:39 +0700

    Add files via upload

---
+++ Java script 1/main.js
@@ -0,0 +1,12 @@
+// console.log("Hello World Internal")
+
+// var varVariable = "Hello from var"
+// console.log(varVariable)
+// var varVariable = "Hello from Var below"
+// console.log(varVariable)
+// var varVariable = "Hello from var again"
+// console.log(varVariable)
+
+// let letVariable = "Hello from let"
+console.log(letVariable)
+let letVariable = "Hello from let below"
